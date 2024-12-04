a9e6376e473c3bbe2bdddc2b83040c4cfcf65f1b
olletidblom <4xnxsrzc@students.codeinstitute.net>
2024-12-04 10:40:34 +0100

    Created the basic layout of the game

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+const rows = 5;
+const cols = 6;
+const board = [];
+
+
+for (let row = 0; row < rows; row++) {
+    board[row] = []
+    for (let col = 0; col < cols; col++) {
+        board[row][col] = null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
